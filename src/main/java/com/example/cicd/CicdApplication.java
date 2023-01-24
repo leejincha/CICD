f13daf2b9801e905cdd141194b1d08fc04236155
leejincha <leejincha@gmail.com>
2023-01-24 14:50:55 +0900

    대기시간 추가

--- src/main/java/com/example/cicd/CicdApplication.java
+++ src/main/java/com/example/cicd/CicdApplication.java
@@ -3,11 +3,14 @@ package com.example.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.lang.Thread.sleep;
+
 @SpringBootApplication
 public class CicdApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(CicdApplication.class, args);
+        sleep(100000);
     }
 
 }
